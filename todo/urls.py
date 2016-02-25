60d32e37d2bb90b576a7865d1415f2d131d0c691
Dylan Vuz <vuzdylan@gmail.com>
2016-02-24 23:18:00 -0500

    step5

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from todo import views
 
 urlpatterns = [
-    url(r'^all/', views.view_todos),
-    url(r'^create/', views.create_todo),
+    url(r'^all/$', views.view_todos),
+    url(r'^completed/$', views.view_completed),
+    url(r'^uncompleted/$', views.view_uncompleted),
+    url(r'^create/$', views.create_todo),
+    url(r'^toggle/(?P<post_id>\d+)/$', views.toggle_todo),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render_to_response, HttpResponseRedirect
-from django.core.context_processors import csrf
+from django.shortcuts import render_to_response, HttpResponseRedirect, get_object_or_404
+from django.template.context_processors import csrf
+from django.core.exceptions import PermissionDenied
 from todo.forms import TodoForm
 from todo.models import Todo
 
 def view_todos(request):
     args = {}
+    args.update(csrf(request))
     args['user'] = request.user
     args['todos'] = Todo.objects.filter(user=request.user)
+    return render_to_response('todos.html', args)
+
+def view_completed(request):
+    args = {}
+    args.update(csrf(request))
+    args['user'] = request.user
+    args['todos'] = Todo.objects.filter(user=request.user, completed=True)
+    return render_to_response('todos.html', args)
+
+def view_uncompleted(request):
+    args = {}
+    args.update(csrf(request))
+    args['user'] = request.user
+    args['todos'] = Todo.objects.filter(user=request.user, completed=False)
     return render_to_response('todos.html', args)
 
 def create_todo(request):
@@ -25,5 +41,12 @@ def create_todo(request):
     args['form'] = form
     return render_to_response('create_todo.html', args)
 
+def toggle_todo(request, post_id):
+    if request.method == 'POST':
+        todo = get_object_or_404(Todo, id=post_id)
+        todo.completed = not todo.completed
+        todo.save()
+        return HttpResponseRedirect('/todo/all')
 
-
+    else:
+        raise PermissionDenied
